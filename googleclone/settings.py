8029b7f2e5c58cd4f88b59613350a27eedf0d687
wilfex <wilfex81@gmail.com>
2022-09-24 21:26:18 +0300

    update

--- googleclone/settings.py
+++ googleclone/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = empty_for_now
+SECRET_KEY ='django-insecure-xd_r1rho8ge$h55=11)w+hw3ber(fjq8db$*t$pe9dolokb&3o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
